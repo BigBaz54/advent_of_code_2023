81f1d90cee1396cca0f02da0a00f5748db24a1bf
BigBaz <louisvincent@famillecapelli.fr>
2023-12-01 22:44:58 -0500

    [+] gitignore

--- python/day1/puzzle1/main.py
+++ python/day1/puzzle1/main.py
@@ -1,9 +1,9 @@
-import calibration_values
+import calibration_value
 
 if __name__ == "__main__":
     with open("my_input.txt", "r") as f:
         lines = f.readlines()
     sum = 0
     for line in lines:
-        sum += calibration_values.calibration_value(line)
+        sum += calibration_value.calibration_value(line)
     print(sum)
