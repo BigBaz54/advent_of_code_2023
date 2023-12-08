f15c42203b87f0a48f22603a3322bde59c3c28ed
BigBaz <louisvincent@famillecapelli.fr>
2023-12-08 01:44:45 -0500

    [+] time

--- python/day8/puzzle2/main.py
+++ python/day8/puzzle2/main.py
@@ -1,8 +1,11 @@
 from min_steps import min_steps, parse_input
+import time
 
 
 if __name__ == '__main__':
     with open('my_input.txt', 'r') as f:
         in_str = f.read()
+    start = time.perf_counter()
     pi = parse_input(in_str)
+    print(time.perf_counter() - start)
     print(min_steps(pi[1], pi[0]))
